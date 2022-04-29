d46e99e6e52ab669c5224a6e6fcf802a1332e8f7
lemangouero-xavier <lemangoueroxavier@gmail.com>
2022-04-29 22:35:08 +0200

    maj moviecontroller

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class MovieController extends AbstractController
     /**
      * @Route("/movie", name="app_movie")
      */
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
+        $movie = $movieRepository->find(1);
+        dd($movie);
         return $this->render('movie/index.html.twig', [
             'controller_name' => 'MovieController',
         ]);
